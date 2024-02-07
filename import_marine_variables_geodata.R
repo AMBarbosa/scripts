c1296c9695ba2c3ecfd064855f33ffe64bf86583
AMBarbosa <AMBarbosa@users.noreply.github.com>
2024-02-07 12:08:33 +0000

    vars -> layers; outputs -> ../outputs

--- import_marine_variables_geodata.R
+++ import_marine_variables_geodata.R
@@ -1,37 +1,37 @@
 library(terra)
 library(geodata)
 
-# (set working directory to source file location?)
+# (set working directory to source file location)
 
 ?geodata::bio_oracle
 
-var_names <- c("Calcite", "Chlorophyll", "Cloud.cover", "Current.Velocity", "Diffuse.attenuation", "Dissolved.oxygen", "Ice.cover", "Ice.thickness", "Iron", "Light.bottom", "Nitrate", "Par", "pH", "Phosphate", "Phytoplankton", "Primary.productivity", "Salinity", "Silicate", "Temperature")
+layer_names <- c("Calcite", "Chlorophyll", "Cloud.cover", "Current.Velocity", "Diffuse.attenuation", "Dissolved.oxygen", "Ice.cover", "Ice.thickness", "Iron", "Light.bottom", "Nitrate", "Par", "pH", "Phosphate", "Phytoplankton", "Primary.productivity", "Salinity", "Silicate", "Temperature")
 
-# currently, geodata::bio_oracle() only supports one variable at a time, so let's loop:
-for (v in var_names) {
-  geodata::bio_oracle(path = "outputs", var = v, stat = ifelse(v == "pH", "", "Mean"), benthic = FALSE)  # check help file for other values of "stat" and "benthic"
+# currently, geodata::bio_oracle() only supports one layeriable at a time, so let's loop:
+for (v in layer_names) {
+  geodata::bio_oracle(path = "../outputs", layer = v, stat = ifelse(v == "pH", "", "Mean"), benthic = FALSE)  # check help file for other values of "stat" and "benthic"
 }
 
-list.files("outputs")
-var_files <- list.files("outputs/bio-oracle", pattern = "\\.tif$", full.names = TRUE)
-var_files
+list.files("../outputs")
+layer_files <- list.files("../outputs/bio-oracle", pattern = "\\.tif$", full.names = TRUE)
+layer_files
 
-# vars <- rast(var_files)  # Error: [rast] extents do not match
+# layers <- terra::rast(layer_files)  # Error: [rast] extents do not match
 
-# exclude the different vars for now:
-vars <- terra::rast(var_files[-grep("pH|calcite|Par", var_files)])
-terra::nlyr(vars)
-terra::plot(vars)
+# exclude the different layers for now:
+layers <- terra::rast(layer_files[-grep("pH|calcite|Par", layer_files)])
+terra::nlyr(layers)
+terra::plot(layers)
 
 # check their extents:
-terra::ext(rast(var_files[grep("pH", var_files)]))
-terra::ext(rast(var_files[grep("calcite", var_files)]))
-terra::ext(rast(var_files[grep("Par", var_files)]))
+terra::ext(rast(layer_files[grep("pH", layer_files)]))
+terra::ext(rast(layer_files[grep("calcite", layer_files)]))
+terra::ext(rast(layer_files[grep("Par", layer_files)]))
 
-# combine, project and append the different vars:
-vars2 <- terra::rast(var_files[grep("pH|calcite|Par", var_files)])
-terra::plot(vars2)
-terra::crs(vars2, proj = TRUE)
-vars2 <- terra::project(vars2, vars)
-vars <- c(vars, vars2)
-terra::nlyr(vars)
+# combine, project and append the different layers:
+layers2 <- terra::rast(layer_files[grep("pH|calcite|Par", layer_files)])
+terra::plot(layers2)
+terra::crs(layers2, proj = TRUE)
+layers2 <- terra::project(layers2, layers)
+layers <- c(layers, layers2)
+terra::nlyr(layers)
